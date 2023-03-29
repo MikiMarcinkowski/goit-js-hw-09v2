61af644e653430f1058839b5a3800341fe025886
mikus151 <mikus151@wp.pl>
2023-03-29 10:15:56 +0200

    task 1 from broken last repo

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+//for better view I changed body height
+const body = document.querySelector('body');
+body.style.height = '400px';
+
+// color changing function
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+stopBtn.disable = true;
+
+let timerId = null;
+
+const startColorChange = () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disable = true;
+  stopBtn.disable = false;
+};
+
+startBtn.addEventListener('click', startColorChange);
+
+const stopColorChange = () => {
+  clearInterval(timerId);
+  startBtn.disable = false;
+  stopBtn.disable = true;
+};
+
+stopBtn.addEventListener('click', stopColorChange);
